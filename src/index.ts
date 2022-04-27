4f233fc8e99449fe87e02058daaead6ee35ec55e
Austin Mayer <a.inoue@churadata.okinawa>
2022-04-27 12:14:14 +0900

    try file streaming

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,34 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import https from "https";
-import { readFileSync } from "fs";
+import { readFileSync, createReadStream } from "fs";
 import multer from "multer";
 
-const upload = multer({ dest: path.resolve(__dirname, "../uploads/") });
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename(req, file, callback) {
+    callback(null, `${new Date().getTime()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  dest: path.resolve(__dirname, "../uploads/"),
+  storage,
+  fileFilter(req, file, callback) {
+    if (["text/csv", "image/png"].includes(file.mimetype)) {
+      callback(null, true);
+    }
+    callback(null, true);
+  },
+});
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+app.use("/public", express.static(path.resolve(__dirname, "../public")));
 
 const server = https.createServer(
   {
@@ -23,9 +43,31 @@ app.get("/", (req, res) => {
   res.render(path.join(__dirname, "index.ejs"), { message: "Hello World" });
 });
 
+app.get("/uploads/:fileName", (req, res) => {
+  console.log(req.params.fileName);
+  const fileStream = createReadStream(
+    path.resolve(__dirname, "../uploads", req.params.fileName)
+  );
+  fileStream.pipe(res);
+});
+
 app.post("/", upload.single("file"), (req, res) => {
   console.log(req.file);
-  res.redirect("/")
+  if (!req.file) {
+    return res.render(path.join(__dirname, "index.ejs"), {
+      message: "Invalid Format",
+    });
+  }
+  res.redirect(`/uploads/${req.file.filename}`);
+});
+
+app.get("/video", (req, res) => {
+  res.render("video.ejs");
+});
+
+app.put("/video", upload.single("file"), (req, res) => {
+  console.log(req.file);
+  res.json({})
 });
 
 const port = 4000;
